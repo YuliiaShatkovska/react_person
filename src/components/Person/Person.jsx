de8fd8f812f527220181867ce536867670ee490f
Yuliia Shatkovska <kushnir.jul@yahoo.com>
2024-09-16 15:07:53 +0300

    add

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,17 @@
-// export const Person = ({ person }) => ();
+import './Person.scss';
+
+export const Person = ({ person }) => {
+  const { name, age, isMarried, partnerName, sex } = person;
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {name}</h2>
+      {age && <p className="Person__age">I am {age}</p>}
+      <p className="Person__partner">
+        {isMarried
+          ? `${partnerName} is ${sex === 'm' ? 'my wife' : 'my husband'}`
+          : 'I am not married'}
+      </p>
+    </section>
+  );
+};
